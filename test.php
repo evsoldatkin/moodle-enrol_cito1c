afba4f90e9d1bfc97ad2197e08bab8d054771337
evsoldatkin <e.v.soldatkin@gmail.com>
2023-05-18 17:12:05 +0300

    fix test for 3.5

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../../config.php';
 global $CFG, $USER;
+require_login();
 require_once $CFG->dirroot.'/lib/externallib.php';
 $USER->ignoresesskey = true;
 $params = [
@@ -11,10 +12,10 @@ $params = [
 $params = [
     'enrolments' => [$params]
 ];
-$result = \core_external\external_api::call_external_function('enrol_cito1c_enrol_users', $params);
+$result = external_api::call_external_function('enrol_cito1c_enrol_users', $params);
 if ($result['error'] === false)
 {
-    $result = \core_external\external_api::call_external_function('enrol_cito1c_unenrol_users', $params);
+    $result = external_api::call_external_function('enrol_cito1c_unenrol_users', $params);
     if ($result['error'] === false)
     {
         echo 'Test passed';
